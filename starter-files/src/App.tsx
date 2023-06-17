7c549a5856c7a62a2997c5510021c62292ea9d30
hridoy00005 <hcb2433@gmail.com>
2023-06-17 09:17:48 +0600

    Second api update

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,70 +1,23 @@
-import { ChangeEvent, useState } from 'react'
-import { optionType } from './types'
+import Search from './components/Search'
+import useForecast from './hooks/useForecast'
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('')
-  const [options, setOptions] = useState<[]>([])
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
 
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
-        process.env.REACT_APP_API_KEY
-      }`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data))
-  }
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim()
-    setTerm(value)
-
-    if (value === '') return
-
-    getSearchOptions(value)
-  }
-
-const onOptionSelect = (option: optionType) => {
-console.log(option.name);
-console.log(option.lat);
-console.log(option.lon);
-}
-
-  //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      <section className="w-full md:max-w-[500px] flex flex-col text-center justify-center items-center p-4 md:px-10 lg:p-24 h-full lg:max-h-[500px]  bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
-        <h1 className="text-4xl font-thin">
-          Weather <span className="font-black">Forecast</span>
-        </h1>
-
-        <p className="text-sm mt-2">
-          Enter below a place you want to know the weather of and select an
-          option from the dropdown
-        </p>
-
-        <div className="relative flex mt-10 md:mt-4">
-          <input
-            type="text"
-            value={term}
-            onChange={onInputChange}
-            className="px-2 py-1 rounded-l-md border-2 border-white"
-          />
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map((option: optionType, index: number) => (
-              <li key={option.name + '-' + index}>
-                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"onClick={() => onOptionSelect(option)} >
-                  {option.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          <button className="px-2 py-1 rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 text-zinc-100 hover:text-zinc-500 cursor-pointer">
-            search
-          </button>
-        </div>
-      </section>
+      {forecast ? (
+        'We have a forecast'
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
